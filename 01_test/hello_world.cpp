ccc327d18ee85dd555a7c0bea51f8b3e54d1db7b
Darptolus <diegoroa90@gmail.com>
2019-09-26 03:47:00 -0400

    2

--- 01_test/hello_world.cpp
+++ 01_test/hello_world.cpp
@@ -9,6 +9,18 @@ int main(){
 
 struct timeval start, end;
 int numOfIter = 1;
+MPI_Init(NULL, NULL);
+		int world_size;
+		MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+		int world_rank;
+		MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+		char node_name[MPI_MAX_PROCESSOR_NAME];
+		int name_len;
+		MPI_Get_processor_name(node_name, &name_len);
+
+if (world_rank == 0){
 
 /* ver. 1: sequential access */
 
@@ -39,19 +51,11 @@ gettimeofday(&end, NULL);
 printf("v2: %lf us\n", (((end.tv_sec * 1000000 + end.tv_usec)	- (start.tv_sec * 1000000 + start.tv_usec)))*1.0/numOfIter);
 
 /* end */
+}
 
 /* ver. 3: MPI */
 
-MPI_Init(NULL, NULL);
-		int world_size;
-		MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-		int world_rank;
-		MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
-		char node_name[MPI_MAX_PROCESSOR_NAME];
-		int name_len;
-		MPI_Get_processor_name(node_name, &name_len);
+MPI_Barrier(MPI_COMM_WORLD);
 
 gettimeofday(&start, NULL);
 
@@ -60,7 +64,7 @@ printf("Hello World from node %s process %d from %d\n", node_name, world_rank, w
 MPI_Barrier(MPI_COMM_WORLD);
 gettimeofday(&end, NULL);
 
-printf("v3: %lf us\n", (((end.tv_sec * 1000000 + end.tv_usec)	- (start.tv_sec * 1000000 + start.tv_usec)))*1.0/numOfIter);
+printf("v3: %lf us from node %s process %d from %d\n", (((end.tv_sec * 1000000 + end.tv_usec)	- (start.tv_sec * 1000000 + start.tv_usec)))*1.0/numOfIter, node_name, world_rank, world_size);
 
 /* end */
 MPI_Finalize();
